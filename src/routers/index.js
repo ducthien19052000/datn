74e09e62f2291bd107a361d73abbb94cfba7d600
Thien ND - F1 -D1 <=>
2020-11-05 17:17:24 +0700

    demo

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Main from '../pages/layout/Main'
 import MainAdmin from '../pages/layout/MainAdmin'
 import Home from '../pages/view/Main/Home'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Route, Switch } from 'react-router-dom';
 import Product from '../pages/view/Main/Product';
 import FoodDetail from '../pages/view/Main/FoodDetail';
 
 const Routers = () => {
     return (
-        <Router>
+        <HashRouter>
             <Switch>
                 <Route path="/admin/:path?/:path?" exact>
                
@@ -34,7 +34,7 @@ const Routers = () => {
                     </Main>
                 </Route>
             </Switch>
-        </Router>
+        </HashRouter>
        
     )
 }
